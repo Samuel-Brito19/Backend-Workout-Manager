cd90d34ba8da7cf679b85ace7182fb59619d7f59
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-07 10:35:11 -0300

    feat: add fastfy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler(request, reply) {
+  return { hello: 'Samuel' }
+})
+
+fastify.listen({ port: 3000 }, (error) => {
+  if (error) {
+    fastify.log.error(error)
+    process.exit(1)
+  }
+})
