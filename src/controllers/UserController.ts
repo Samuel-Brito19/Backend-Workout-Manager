7474906982ab7e31b3862a13e74d66c032e8312c
Samuel Brito <sambriton7@gmail.com>
2023-10-10 18:51:55 -0300

    Creating a add user function

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,11 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '../../database'
 
+interface CreateUserRequestBody {
+  name: string;
+  email: string;
+  password: string;
+}
 class UserController {
   static async index(request: FastifyRequest, reply: FastifyReply) {
     const users = await prisma.user.findMany({
@@ -11,6 +16,27 @@ class UserController {
       },
     })
     return users
+  }
+
+  static async store(request: FastifyRequest, reply: FastifyReply) {
+
+    const {name, email, password} = request.body as CreateUserRequestBody
+
+    const userExists = await prisma.user.findUnique({where: {email}})
+
+    if(userExists) {
+      return reply.status(400).send({error: "user already exists!"})
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    })
+
+    return newUser
   }
 }
 
