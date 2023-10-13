4ed130f54f22ba07a735481d6fc232618b334258
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-13 16:23:29 -0300

    feat: add type type to fastify request

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,17 +1,15 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '../../database'
-import {hash} from 'bcryptjs'
+import { hash } from 'bcryptjs'
 
 interface CreateUserRequestBody {
-  name: string;
-  email: string;
-  password: string;
+  name: string
+  email: string
+  password: string
 }
 
-
-
 class UserController {
-  static async index(request: FastifyRequest, reply: FastifyReply) {
+  static async index() {
     const users = await prisma.user.findMany({
       select: {
         id: true,
@@ -22,46 +20,52 @@ class UserController {
     return users
   }
 
-  static async store(request: FastifyRequest, reply: FastifyReply) {
+  static async store(
+    request: FastifyRequest<{
+      Body: CreateUserRequestBody
+    }>,
+    reply: FastifyReply,
+  ) {
+    const { name, email, password } = request.body
 
-    const {name, email, password} = request.body as CreateUserRequestBody
-
-    if(!email.includes('@')) {
-      return reply.status(400).send({error: "You're missing the '@'!"})
+    if (!email.includes('@')) {
+      return reply.status(400).send({ error: "You're missing the '@'!" })
     }
 
-    if(password.length < 6) {
-      return reply.status(400).send({error: 'Password too short!'})
+    if (password.length < 6) {
+      return reply.status(400).send({ error: 'Password too short!' })
     }
 
-    if(name === null || name === undefined || name.trim() === '') {
-      return reply.status(400).send({error: 'Invalid name!'})
+    if (name === null || name === undefined || name.trim() === '') {
+      return reply.status(400).send({ error: 'Invalid name!' })
     } else if (email === null || email === undefined || email.trim() === '') {
-      return reply.status(400).send({error: 'Invalid email!'})
-    } else if (password === null || password === undefined || password.trim() === '') {
-      return reply.status(400).send({error: 'Invalid password!'})
+      return reply.status(400).send({ error: 'Invalid email!' })
+    } else if (
+      password === null ||
+      password === undefined ||
+      password.trim() === ''
+    ) {
+      return reply.status(400).send({ error: 'Invalid password!' })
     }
 
-    const userExists = await prisma.user.findUnique({where: {email}})
+    const userExists = await prisma.user.findUnique({ where: { email } })
 
-    if(userExists) {
-      return reply.status(400).send({error: "user already exists!"})
+    if (userExists) {
+      return reply.status(400).send({ error: 'user already exists!' })
     }
 
-    const hashPassword = await hash(password,8 )
+    const hashPassword = await hash(password, 8)
 
     const newUser = await prisma.user.create({
       data: {
         name,
         email,
-        password: hashPassword
-      }
+        password: hashPassword,
+      },
     })
 
     return newUser
   }
-
-  
 }
 
 export default UserController
