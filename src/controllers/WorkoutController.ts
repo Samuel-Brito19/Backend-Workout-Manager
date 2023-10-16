8b9746724810fcc4d209ee0f8be80a3ac5f785be
Samuel Brito <sambriton7@gmail.com>
2023-10-16 19:23:15 -0300

    Creating our Workout Controller

--- src/controllers/WorkoutController.ts
+++ src/controllers/WorkoutController.ts
@@ -2,7 +2,18 @@ import { prisma } from "../../database";
 
 class WorkoutController {
 
-    static ass
+    static async index() {
+
+        const workout = await prisma.workout.findMany({
+            select: {
+                id: true,
+                title: true,
+                user: true,
+                userId: true
+            }
+        })
+        return workout
+    }
 }
 
 export default WorkoutController
