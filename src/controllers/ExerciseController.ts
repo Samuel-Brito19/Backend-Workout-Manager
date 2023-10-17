2a79a6ae5e3285452b1626eaf4728ab92383702d
Samuel Brito <sambriton7@gmail.com>
2023-10-17 20:25:17 -0300

    Creating index function in our ExerciseController

---
+++ src/controllers/ExerciseController.ts
@@ -0,0 +1,38 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "../../database";
+
+interface ParamsExercise {
+    workoutId: number,
+    id: number
+}
+
+interface RequestExersice {
+    name: string,
+    sets: number,
+    repetitions: number,
+}
+
+
+class ExerciseController {
+
+    static async index(request: FastifyRequest<{Params: ParamsExercise, Body: RequestExersice}>, reply: FastifyReply) {
+
+        const {workoutId} = request.params
+
+        const workoutIdNumber = Number(workoutId)
+
+        if(!workoutIdNumber) {
+            return reply.status(400).send({erro: "Workout doesn't exist!"})
+        }
+
+        const exercises = await prisma.exercise.findMany({
+            where: {
+                workoutId: workoutIdNumber
+            }
+        })
+
+        return exercises
+    }
+}
+
+export default ExerciseController
