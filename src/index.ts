46a02c4bd5932166b5b232641ac4511d435744c3
Samuel Brito <sambriton7@gmail.com>
2023-10-23 10:47:14 -0300

    Finishing our project

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,7 @@ import UserController from './controllers/UserController'
 import WorkoutController from './controllers/WorkoutController'
 import ExerciseController from './controllers/ExerciseController'
 import { AuthController } from './controllers/AuthController'
-<<<<<<< HEAD
-import authenticateJWT from './middlewares/middlewareFunction'
-=======
 import { authenticateJWT } from './middlewares/auth'
->>>>>>> 5b064ff3b2370d61d5ad21d138d130fa66e4c088
 
 const fastify = Fastify({
   logger: true,
@@ -18,16 +14,12 @@ fastify.get('/users', UserController.index)
 fastify.post('/users', UserController.store)
 fastify.delete('/users/:id', UserController.delete)
 
-<<<<<<< HEAD
-fastify.get('/users/:userId/workouts', {preHandler: [authenticateJWT]}, WorkoutController.index)
-fastify.post('/users/:userId/workouts', WorkoutController.store)
-=======
+
 fastify.get('/users/:userId/workouts', WorkoutController.index)
 fastify.post('/users/:userId/workouts', {
   preHandler: authenticateJWT,
   handler: WorkoutController.store,
 })
->>>>>>> 5b064ff3b2370d61d5ad21d138d130fa66e4c088
 fastify.delete('/users/:userId/workouts/:id', WorkoutController.delete)
 
 fastify.get('/users/workouts/:workoutId/exercises', ExerciseController.index)
